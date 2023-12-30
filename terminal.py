e15a1c65d111209845de57293c69663bb94f2e0c
khakomaki <hakomaki.karri@gmail.com>
2023-12-30 18:59:48 +0200

    better help and info, print to logging in terminal

--- terminal.py
+++ terminal.py
@@ -1,4 +1,10 @@
 from connection import *
+import logging
+
+logging.basicConfig(        # sets up logging configuration
+    level=logging.INFO,
+    format="%(message)s"
+    )
 
 class NPCChatter:
 
@@ -48,17 +54,41 @@ class NPCChatter:
         self.connection.send_chat_message(message)
 
     def print_info(self, *_):
-        print("========== Chatter settings info ==========")
-        print(f"Same word count: {self.same_word_count}")
-        print(f"History size: {self.history_size}")
-        print(f"Threshold: {self.threshold}")
-        print("========================================")
+        attributes = [
+            ("Same word count", str(self.same_word_count)),
+            ("History size", str(self.history_size)),
+            ("Threshold", str(self.threshold))
+        ]
+        self.print_text_box("Chatter settings info", attributes)
 
     def print_help(self, *_):
-        print("========== Available commands ==========")
+        attributes = []
         for command, command_function in self.commands.items():
-            print(f"{command} - {command_function.get_help_text()}")
-        print("========================================")
+            attributes.append((command, command_function.get_help_text()))
+
+        self.print_text_box("Available commands", attributes)
+
+    def print_text_box(self, title: str, names_and_values: list[tuple[str, str]]):
+        # maximum attribute name & line lengths for styling
+        max_name_length = max(len(name) for name, _ in names_and_values)
+        max_line_length = max(len(name.ljust(max_name_length)) + 3 + len(value) for name, value in names_and_values)
+
+        # title bar (makes sure that has at least some bars)
+        title_bar = f"=== {title} ==="
+        side_line_count = int((max_line_length - len(title_bar)) / 2)
+        if 0 < side_line_count:
+            side_lines = '=' * side_line_count
+            title_bar = f"{side_lines}{title_bar}{side_lines}"
+        else:
+            max_line_length = len(title_bar)
+        logging.info(title_bar)
+
+        # attribute names and values
+        for name, value in names_and_values:
+            logging.info(f"{name.ljust(max_name_length)} - {value}")
+
+        # footer bar
+        logging.info('=' * max_line_length)
 
     def set_num_attr(self, attribute_name, *args):
         if not (hasattr(self, attribute_name)):
@@ -71,22 +101,22 @@ class NPCChatter:
             raise NPCError("You forgot to give the value!")
 
         user_value = str(args[0])   # resf of values are ignored
-        if not user_value.isdigit():
+        try:
+            user_value = int(user_value)
+            if user_value < 1:
+                raise NPCError(f"Given value '{user_value}' is invalid!")
+        except ValueError:
             raise NPCError(f"Given argument '{user_value}' isn't a number!")
 
-        user_value = int(user_value)
-        if user_value < 1:
-            raise NPCError(f"Given value '{user_value}' is invalid!")
-
         setattr(self, attribute_name, user_value)
-        print(f"{attribute_name.upper()} set to [{getattr(self, attribute_name)}]")
+        logging.info(f"{attribute_name.upper()} set to [{getattr(self, attribute_name)}]")
 
     def exit(self):
         self.disconnect()
         exit()
 
     def run(self):
-        print(f"Welcome to NPC-terminal. 'EXIT' to close, 'HELP' for list of commands.")
+        logging.info(f"Welcome to NPC-terminal. 'EXIT' to close, 'HELP' for list of commands.")
 
         while True:
             try:
@@ -105,16 +135,16 @@ class NPCChatter:
                     command = self.commands.get(user_command)
                     command.execute(*user_arguments)
                 else:
-                    print(f"Didn't find any commands named '{user_command}'!")
+                    logging.warning(f"Didn't find any commands named '{user_command}'!")
 
             except NPCError as error:
-                print(error)
+                logging.error(error)
             
             except TwitchConnectionError as error:
-                print(error)
+                logging.error(error)
 
             except Exception as error:
-                print(f"Unexpected error: {error}")
+                logging.error(f"Unexpected error: {error}")
 
 
 class NPCCommand:
